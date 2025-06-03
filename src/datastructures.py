dee728cd086cdd1528a85d4dd1016a0badb29e72
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-03 00:45:34 +0000

    build add member the first task

--- src/datastructures.py
+++ src/datastructures.py
@@ -16,6 +16,20 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
@@ -28,6 +42,13 @@ class FamilyStructure:
     def add_member(self, member):
         ## You have to implement this method
         ## Append the member to the list of _members
+        if 'id' not in member:
+            # if they arent, generate id for this member
+            member.update(
+                id=self._generate_id()
+            )
+        # append the member to the members list
+        # self._members.append(member)    
         pass
 
     def delete_member(self, id):
